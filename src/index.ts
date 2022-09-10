77ed45fbaee8e7f74a5b69b5668f190b8fed0436
MathieuGermain <gtavoverride@gmail.com>
2022-09-10 03:28:56 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: 'assets/images/icon.png',
         webPreferences: {
             preload: join(__dirname, 'preload.js'),
         },
